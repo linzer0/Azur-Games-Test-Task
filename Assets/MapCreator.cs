80834a4f4a7c1a4c0578fb0230aa205b182325d9
Linar Khilazhev <l.khilajev@pologames.org>
2021-02-08 19:31:38 +0300

    Map creation added

---
+++ Assets/MapCreator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject TilePrefab;
+
+    [SerializeField] private float TileOffset = 10f;
+    
+    private int MapSize;
+
+    public void CreateMap(int mapSize)
+    {
+        MapSize = mapSize;
+
+        for (int i = 0; i < mapSize; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                var spawnPosition = new Vector3(i * TileOffset, 0, j * TileOffset);
+                var tile = Instantiate(TilePrefab, spawnPosition, Quaternion.identity);
+                tile.transform.SetParent(gameObject.transform, true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/SimulationComponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gameplay
+{
+    public class SimulationComponent : MonoBehaviour
+    {
+        private int MapSize;
+        private int AnimalsAmount;
+        private int Speed;
+
+        [SerializeField] private Button StartButton;
+
+        [SerializeField] private Slider MapSizeSlider;
+        [SerializeField] private Slider AnimalsAmountSlider;
+        [SerializeField] private Slider SpeedValueSlider;
+
+        [SerializeField] private MapCreator MapCreator;
+
+
+        void Start()
+        {
+            StartButton.onClick.AddListener(GetValuesFromSlider);
+        }
+
+        void GetValuesFromSlider()
+        {
+            MapSize = (int) MapSizeSlider.value;
+            AnimalsAmount = (int) AnimalsAmountSlider.value;
+            Speed = (int) SpeedValueSlider.value;
+            
+            MapCreator.CreateMap(MapSize);
+
+            StartButton.onClick.RemoveListener(GetValuesFromSlider);
+        }
+    }
+}
