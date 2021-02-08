88c37b10844af9a249173056ccbe2710f3321725
Linar Khilazhev <l.khilajev@pologames.org>
2021-02-08 14:41:43 +0300

    Initial commit

---
+++ Assets/Scripts/UI/OnSliderChange.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class OnSliderChange : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI Text;
+        [SerializeField] private Slider Slider;
+
+        void Start()
+        {
+            Slider.onValueChanged.AddListener(Temp);
+        }
+
+        void Temp(float sliderValue)
+        {
+            Text.SetText(sliderValue.ToString());
+        }
+    }
+}
