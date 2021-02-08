d49e9ff07f837459494a0067df06b6cb6a5b8659
Linar Khilazhev <l.khilajev@pologames.org>
2021-02-08 19:12:19 +0300

    Animal amount depends from Map Size(N) slider

---
+++ Assets/Scripts/UI/DependsMaxValueFromAnotherSlider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class DependsMaxValueFromAnotherSlider : MonoBehaviour
+    {
+        [SerializeField] private Slider DependsFromSlider;
+        [SerializeField] private Slider CurrentFromSlider;
+
+        void Start()
+        {
+            DependsFromSlider.onValueChanged.AddListener(SetMaximumValue);
+        }
+
+        void SetMaximumValue(float value)
+        {
+            CurrentFromSlider.maxValue = (int) value * value / 2;
+            CurrentFromSlider.onValueChanged.Invoke(CurrentFromSlider.value);
+        }
+    }
+}
